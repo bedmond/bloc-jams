71e655380c458d48c72c176b0fe3577bac362e25
Brian Edmond <vagrant@precise32.(none)>
2015-08-03 23:41:47 +0000

    Changed slider to update (music location) data on change.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,11 +12,11 @@ var albumPicasso = {
   albumArtUrl: '/images/album-placeholder.png',
 
   songs: [
-      { name: 'Blue', length: '4:26', audioUrl: '/music/placeholders/blue' },
-      { name: 'Green', length: '3:14', audioUrl: '/music/placeholders/green' },
-      { name: 'Red', length: '5:01', audioUrl: '/music/placeholders/red' },
-      { name: 'Pink', length: '3:21', audioUrl: '/music/placeholders/pink' },
-      { name: 'Magenta', length: '2:15', audioUrl: '/music/placeholders/magenta' },
+      { name: 'Blue', length: 163.38, audioUrl: '/music/placeholders/blue' },
+      { name: 'Green', length: 105.66, audioUrl: '/music/placeholders/green' },
+      { name: 'Red', length: 270.14, audioUrl: '/music/placeholders/red' },
+      { name: 'Pink', length: 154.81, audioUrl: '/music/placeholders/pink' },
+      { name: 'Magenta', length: 375.92, audioUrl: '/music/placeholders/magenta' },
     ]
 };
 
@@ -34,20 +34,43 @@ blocJams.directive('slider', ['$document', function($document) {
     return offsetXPercent;
   }
 
+  var numberFromValue = function(value, defaultValue) {
+    if (typeof value === 'number') {
+      return value;
+    }
+
+    if (typeof value === 'undefined') {
+      return defaultValue;
+    }
+
+    if (typeof value === 'string') {
+      return Number(value);
+    }
+  }
+
   return {
     templateUrl: '/templates/directives/slider.html',
     replace: true,
     restrict: 'E',
-    scope: {}, // Creates a scope that exists only in this directive.
+    scope: { // Creates a scope that exists only in this directive.
+      onChange: '&'
+    },
     link: function(scope, element, attributes) {
       // These values represent the progress into the song/volume bar, and its max value.
       // For now, we're supplying arbitrary initial and max values.
       scope.value = 0;
-      scope.max = 200;
+      scope.max = 100;
       var $seekBar = $(element);
 
+      console.log(attributes);
+      attributes.$observe('value', function(newValue) {
+        scope.max = numberFromValue(newValue, 100) || 100;
+      });
+
       var percentString = function () {
-        var percent = Number(scope.value) / Number(scope.max) * 100;
+        var value = scope.value || 0;
+        var max = scope.max || 100;
+        percent = value / max * 100;
         return percent + "%";
       }
 
@@ -62,6 +85,7 @@ blocJams.directive('slider', ['$document', function($document) {
       scope.onClickSlider = function(event) {
         var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
         scope.value = percent * scope.max;
+        notifyCallback(scope.value);
       }
 
       scope.trackThumb = function() {
@@ -69,6 +93,7 @@ blocJams.directive('slider', ['$document', function($document) {
           var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
           scope.$apply(function() {
             scope.value = percent * scope.max;
+            notifyCallback(scope.value);
           });
         });
 
@@ -77,6 +102,12 @@ blocJams.directive('slider', ['$document', function($document) {
           $document.unbind('mousemove.thumb');
           $document.unbind('mouseup.thumb');
         });
+      };
+
+      var notifyCallback = function(newValue) {
+        if (typeof scope.onChange === 'function') {
+          scope.onChange({value: newValue});
+        }
       };
     }
   }; 
@@ -229,6 +260,13 @@ blocJams.service('SongPlayer', function() {
       }
       var song = this.currentAlbum.songs[currentTrackIndex];
       this.setSong(this.currentAlbum, song);
+    },
+    seek: function(time) {
+      // Checks to make sure that a sound file is playing before seeking.
+      if (currentSoundFile) {
+        // Uses a Buzz method to set the time of the song.
+        currentSoundFile.setTime(time);
+      }
     },
     setSong: function(album, song) {
       if (currentSoundFile) {
